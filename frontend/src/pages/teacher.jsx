7d18fa79f24fdddbf2d1fa63f70127ac821203c7
Ebbsteer <Ebba.elversson@gmail.com>
2023-11-29 10:21:48 +0100

    Change default css

---
+++ frontend/src/pages/teacher.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+const ActiveAPL = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+
+}
+
+const TodayMood = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+
+}
+
+const TodayReport = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+
+}
+
+const ActiveCompanies = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+
+}
+
+const ActiveStudents = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+
+}
+
+const CreateNewObject = () => {
+
+    return (
+        <>
+        
+        </>
+    );
+
+}
+
+
+export const TeacherPage = () => {
+
+    return (
+        <>
+            <div className='bg-neutral-700 bg-opacity-50 p-10 md:container'>
+                <div className='bg-neutral-700 container w-full h-full'>
+                    hello
+                </div>
+            </div>
+        </>
+    );
+}
+                
