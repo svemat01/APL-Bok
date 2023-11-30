b99105788dba0523a69e611ec3e21703645ea4a0
Salah.Alhassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-11-30 09:07:13 +0100

    fix

--- frontend/src/pages/CreateStudent.jsx
+++ frontend/src/pages/CreateStudent.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 
 const Navbar = ({ toggleMenu }) => {
     return (
-        <nav className="fixed inset-x-0 top-0 flex items-center justify-between p-2 md:p-4 lg:p-6 h-16" style={{ backgroundColor: '#581C87' }}>
+        <nav className="fixed inset-x-0 top-0 flex items-center justify-between p-2" style={{ backgroundColor: '#581C87' }}>
             <button onClick={toggleMenu} className="lg:hidden text-white">
                 <div className="w-4 h-px bg-transparent mb-1" style={{ border: '1px solid #fff' }}></div>
                 <div className="w-4 h-px bg-transparent mb-1" style={{ border: '1px solid #fff' }}></div>
                 <div className="w-4 h-px bg-transparent" style={{ border: '1px solid #fff' }}></div>
             </button>
-            <h1 className="text-lg md:text-2xl lg:text-3xl text-white">Tracify</h1>
+            <h1 className="text-lg text-white lg:text-2xl">Tracify</h1>
         </nav>
     );
 }
@@ -45,97 +45,93 @@ const CreateStudent = () => {
     }
 
     return (
-        <div className="mt-16 container mx-auto bg-neutral-700 bg-opacity-50 text-white p-4 md:p-8 lg:p-12 rounded-lg">
-            <div className='container bg-neutral-700 rounded-lg p-4 md:p-8 lg:p-12 text-center'>
-                <div className=''>
-                    <h2 className="mb-4 text-lg md:text-xl lg:text-2xl font-bold">Personinformation</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-                        <input
-                            className="mb-2 p-2 border rounded w-full"
-                            type="text"
-                            placeholder="Förnamn"
-                            value={firstName}
-                            onChange={(e) => setFirstName(e.target.value)}
-                        />
-                        <input
-                            className="mb-2 p-2 border rounded w-full"
-                            type="text"
-                            placeholder="Efternamn"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                        />
-                    </div>
-                    <input
-                        className="w-full mb-4 p-2 border rounded"
-                        type="date"
-                        value={birthDate}
-                        max={getCurrentDate()}
-                        onChange={(e) => setBirthDate(e.target.value)}
-                    />
-                    <h2 className="mb-2 text-lg md:text-xl lg:text-2xl font-bold">Kontaktinformation</h2>
-                    <input
-                        className="w-full mb-2 p-2 border rounded"
-                        type="email"
-                        placeholder="E-post"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        className="w-full mb-2 p-2 border rounded"
-                        type="text"
-                        placeholder="Telefonnummer"
-                        value={phoneNumber}
-                        onChange={handleNumberInput}
-                    />
-                    <input
-                        className="w-full mb-4 p-2 border rounded"
-                        type="text"
-                        placeholder="Adress"
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                    />
-                    <h2 className="mb-2 text-lg md:text-xl lg:text-2xl font-bold">Skola</h2>
-                    <input
-                        className="w-full mb-2 p-2 border rounded"
-                        type="text"
-                        placeholder="Skolnamn"
-                        value={schoolName}
-                        onChange={(e) => setSchoolName(e.target.value)}
-                    />
-                    <input
-                        className="w-full mb-2 p-2 border rounded"
-                        type="text"
-                        placeholder="Grupper"
-                        value={groups}
-                        onChange={(e) => setGroups(e.target.value)}
-                    />
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mb-4">
-                        <input
-                            className="mb-2 p-2 border rounded w-full"
-                            type="text"
-                            placeholder="APL Ansvarig"
-                            value={aplResponsible}
-                            onChange={(e) => setAplResponsible(e.target.value)}
-                        />
-                        <input
-                            className="mb-2 p-2 border rounded w-full"
-                            type="text"
-                            placeholder="Mentor"
-                            value={mentor}
-                            onChange={(e) => setMentor(e.target.value)}
-                        />
-                    </div>
-                    <button
-                        className="p-2 mt-4 rounded"
-                        style={{ backgroundColor: '#581C87' }}
-                        onClick={() => {
-                            // Spara studentinformationen
-                        }}
-                    >
-                        Spara
-                    </button>
-                </div>
+        <div className="flex flex-col items-center justify-center bg-black text-white p-2">
+            <h2 className="mb-2 text-lg font-bold">Personinformation</h2>
+            <div className="flex justify-between w-full mb-2">
+                <input
+                    className="flex-1 mb-1 p-1 mr-1 border rounded"
+                    type="text"
+                    placeholder="Förnamn"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                    className="flex-1 mb-1 p-1 border rounded"
+                    type="text"
+                    placeholder="Efternamn"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                />
             </div>
+            <input
+                className="w-full mb-2 p-1 border rounded"
+                type="date"
+                value={birthDate}
+                max={getCurrentDate()}
+                onChange={(e) => setBirthDate(e.target.value)}
+            />
+            <h2 className="mb-2 text-lg font-bold">Kontaktinformation</h2>
+            <input
+                className="w-full mb-2 p-1 border rounded"
+                type="email"
+                placeholder="E-post"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                className="w-full mb-2 p-1 border rounded"
+                type="text"
+                placeholder="Telefonnummer"
+                value={phoneNumber}
+                onChange={handleNumberInput}
+            />
+            <input
+                className="w-full mb-2 p-1 border rounded"
+                type="text"
+                placeholder="Adress"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+            />
+            <h2 className="mb-2 text-lg font-bold">Skola</h2>
+            <input
+                className="w-full mb-2 p-1 border rounded"
+                type="text"
+                placeholder="Skolnamn"
+                value={schoolName}
+                onChange={(e) => setSchoolName(e.target.value)}
+            />
+            <input
+                className="w-full mb-2 p-1 border rounded"
+                type="text"
+                placeholder="Grupper"
+                value={groups}
+                onChange={(e) => setGroups(e.target.value)}
+            />
+            <div className="flex justify-between w-full mb-2">
+                <input
+                    className="flex-1 mb-1 p-1 mr-1 border rounded"
+                    type="text"
+                    placeholder="APL Ansvarig"
+                    value={aplResponsible}
+                    onChange={(e) => setAplResponsible(e.target.value)}
+                />
+                <input
+                    className="flex-1 mb-1 p-1 border rounded"
+                    type="text"
+                    placeholder="Mentor"
+                    value={mentor}
+                    onChange={(e) => setMentor(e.target.value)}
+                />
+            </div>
+            <button
+                className="p-1 mt-2 rounded"
+                style={{ backgroundColor: '#581C87' }}
+                onClick={() => {
+                    // Spara studentinformationen
+                }}
+            >
+                Spara
+            </button>
         </div>
     );
 }
