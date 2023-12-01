789039a7a4c8d10e530790a42f8e2b6824f7ef4f
Salah.Alhassun <142593904+Salah9320Alhassun@users.noreply.github.com>
2023-12-01 12:18:15 +0100

    Auto stash before merge of "master" and "origin/master"

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,36 +12,46 @@ const LoginForm = () => {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <h2 className="mb-4 text-2xl font-bold text-white">Login</h2>
-            <input 
-                className="mb-2 p-2 border rounded" 
-                type="text" 
-                placeholder="Username" 
-                value={username} 
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input 
-                className="mb-2 p-2 border rounded" 
-                type="password" 
-                placeholder="Password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button className="p-2 border-2 border-#581C87 rounded" style={{backgroundColor: 'white', color: '#581C87'}} type="submit">Login</button>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+            <h2 className="mb-4 text-2xl font-bold">Login</h2>
+            <form onSubmit={handleSubmit} className="w-full max-w-sm">
+                <div className="mb-4">
+                    <input
+                        className="w-full p-2 border rounded"
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <input
+                        className="w-full p-2 border rounded"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button
+                    className="w-full p-2 border-2 rounded"
+                    style={{ backgroundColor: 'white', color: '#581C87' }}
+                    type="submit"
+                >
+                    Login
+                </button>
+            </form>
         </div>
     );
 }
 
-
 const Navbar = () => {
     return (
-        <nav className="fixed inset-x-0 top-0 flex items-center justify-center p-4" style={{backgroundColor: '#581C87'}}>
+        <nav className="fixed inset-x-0 top-0 flex items-center justify-center p-4" style={{ backgroundColor: '#581C87' }}>
             <h1 className="text-4xl text-white">Tracify</h1>
         </nav>
     );
 }
-
 
 export const Welcome = () => {
     return (
